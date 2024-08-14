b42496f8f770568fc9db7686239da63072f13e93
john.cintineo <john.cintineo@noaa.gov>
2024-08-14 13:14:40 +0000

    added non-pyproj calculation of lat/lon

--- src/utils.py
+++ src/utils.py
@@ -547,3 +547,44 @@ def plax(
             pickle.dump(outdata, open(outfile, "wb"))
             logging.info(f"Wrote out {outfile}")
 
+####################################################################################################
+def calculate_abi_degrees(file_id):
+
+    """
+    Calculate latitude and longitude from GOES ABI fixed grid projection data
+    GOES ABI fixed grid projection is a map projection relative to the GOES satellit
+    Units: latitude in °N (°S < 0), longitude in °E (°W < 0)
+    See GOES-R Product User Guide (PUG) Volume 5 (L2 products) Section 4.2.8 for details & example of calculation
+    "file_id" is an ABI L1b or L2 .nc file opened using the netCDF4 library
+    This function was created by NOAA/NESDIS/STAR Aerosols and Atmospheric Composition Science Team.
+    """
+    
+    # Read in GOES ABI fixed grid projection variables and constants
+    x_coordinate_1d = file_id.variables['x'][:]  # E/W scanning angle in radians
+    y_coordinate_1d = file_id.variables['y'][:]  # N/S elevation angle in radians
+    projection_info = file_id.variables['goes_imager_projection']
+    lon_origin = projection_info.longitude_of_projection_origin
+    H = projection_info.perspective_point_height+projection_info.semi_major_axis
+    r_eq = projection_info.semi_major_axis
+    r_pol = projection_info.semi_minor_axis
+    
+    # Create 2D coordinate matrices from 1D coordinate vectors
+    x_coordinate_2d, y_coordinate_2d = np.meshgrid(x_coordinate_1d, y_coordinate_1d)
+    
+    # Equations to calculate latitude and longitude
+    lambda_0 = (lon_origin*np.pi)/180.0  
+    a_var = np.power(np.sin(x_coordinate_2d),2.0) + (np.power(np.cos(x_coordinate_2d),2.0)*(np.power(np.cos(y_coordinate_2d),2.0)+(((r_eq*r_eq)/(r_pol*r_pol))*np.power(np.sin(y_coordinate_2d),2.0))))
+    b_var = -2.0*H*np.cos(x_coordinate_2d)*np.cos(y_coordinate_2d)
+    c_var = (H**2.0)-(r_eq**2.0)
+    r_s = (-1.0*b_var - np.sqrt((b_var**2)-(4.0*a_var*c_var)))/(2.0*a_var)
+    s_x = r_s*np.cos(x_coordinate_2d)*np.cos(y_coordinate_2d)
+    s_y = - r_s*np.sin(x_coordinate_2d)
+    s_z = r_s*np.cos(x_coordinate_2d)*np.sin(y_coordinate_2d)
+    
+    # Ignore numpy errors for sqrt of negative number; occurs for GOES-16 ABI CONUS sector data
+    np.seterr(all='ignore')
+    
+    abi_lat = (180.0/np.pi)*(np.arctan(((r_eq*r_eq)/(r_pol*r_pol))*((s_z/np.sqrt(((H-s_x)*(H-s_x))+(s_y*s_y))))))
+    abi_lon = (lambda_0 - np.arctan(s_y/(H-s_x)))*(180.0/np.pi)
+    
+    return abi_lat, abi_lon
